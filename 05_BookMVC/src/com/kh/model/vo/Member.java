1abd67ce4cc651cc30acc4dd41049d10249628bc
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-07-05 17:51:15 +0900

    Create Member.java

---
+++ 05_BookMVC/src/com/kh/model/vo/Member.java
@@ -0,0 +1,60 @@
+package com.kh.model.vo;
+
+import java.sql.Date;
+
+public class Member {
+
+	private int memberNo;
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	private char status;
+	private Date enrollDate;
+	public Member() {
+		
+	}
+	public Member(String id, String password, String name) {
+		//채워야댐
+		this.memberId = id;
+		this.memberPwd = password;
+		this.memberName = name;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public char getStatus() {
+		return status;
+	}
+	public void setStatus(char status) {
+		this.status = status;
+	}
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+	
+	
+}
