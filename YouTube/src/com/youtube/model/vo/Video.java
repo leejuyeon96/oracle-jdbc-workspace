8208d50ea3f91ebbbd125b91889f3053e8c2c94b
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-07-07 17:14:28 +0900

    Create Video.java

---
+++ YouTube/src/com/youtube/model/vo/Video.java
@@ -0,0 +1,17 @@
+package com.youtube.model.vo;
+
+import java.util.Date;
+
+public class Video {
+	private int videoCode;
+    private String videoTitle;
+    private String videoDesc;
+    private Date videoDate;
+    private int videoViews;
+    private String videoUrl;
+    private String videoPhoto;
+    private Category category;
+//    CHANNEL_CODE NUMBER,
+//    MEMBER_ID VARCHAR2(200)
+
+}
