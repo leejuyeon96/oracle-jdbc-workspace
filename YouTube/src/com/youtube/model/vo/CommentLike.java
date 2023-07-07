5aba2861564f1d4b6cbede48e96cce8b08eaf783
leejuyeon96 <133620354+leejuyeon96@users.noreply.github.com>
2023-07-07 17:14:19 +0900

    Create CommentLike.java

---
+++ YouTube/src/com/youtube/model/vo/CommentLike.java
@@ -0,0 +1,11 @@
+package com.youtube.model.vo;
+
+import java.util.Date;
+
+public class CommentLike {
+	private int commLikeCode;
+    private Date commLikeDate;
+    private VideoComment videoComment;
+    private Member member;
+
+}
